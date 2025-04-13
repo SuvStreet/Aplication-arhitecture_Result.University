8ad23582fc5b069d00ecc9679f44bd2f2851c915
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-04-13 20:33:44 +0300

    feat(Detail): Реализовать код по методологии FSD.

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import axios from 'axios'
 
-export const Detail = () => {
+import { Location, Characters, Episode } from '@shared/ui'
+
+import { BASE_URL } from '@shared/constants/api'
+
+export function Detail() {
+  const { id, category } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState({})
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    detailData()
+  }, [id, category])
+
+  const detailData = async () => {
+    setIsLoading(true)
+
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${category}/${id}`)
+
+      setData(data)
+      setIsLoading(false)
+    } catch (error) {
+      if (error.response?.status === 404) {
+        return navigate(`/${category}`, { replace: true })
+      }
+      setIsLoading(false)
+    }
+  }
+
+  const renderCategoryComponent = () => {
+    switch (category) {
+      case 'character':
+        return <Characters data={data} />
+      case 'location':
+        return <Location data={data} />
+      case 'episode':
+        return <Episode data={data} />
+      default:
+        return null
+    }
+  }
+
+  if (Object.keys(data).length === 0 && !isLoading) return <p>Ой, тут пусто...</p>
+
   return (
-    <div>Detail</div>
+    <>
+      {isLoading ? <p>Загрузка данных...</p> : <>{renderCategoryComponent()}</>}
+    </>
   )
 }
---
+++ src/shared/ui/components/Location/Location.jsx
@@ -0,0 +1,16 @@
+export function Location({ data }) {
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <ul className="list">
+        <li>
+          <span style={{ fontWeight: 'bold' }}>Тип:</span> {data.type}
+        </li>
+        <li>
+          <span style={{ fontWeight: 'bold' }}>Расположение:</span>{' '}
+          {data.dimension}
+        </li>
+      </ul>
+    </div>
+  )
+}
